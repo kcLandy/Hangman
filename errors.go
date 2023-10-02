89236f82a83a25dce695fd304ec959bbdc956291
Lucas Martinez <lucas.martinez@ynov.com>
2023-10-02 11:33:46 +0200

    errors

---
+++ errors.go
@@ -0,0 +1,13 @@
+package main
+
+func errors() {
+	if a != Word {
+	} else {
+		Printf("Vous avez commis une erreur ! Encore 6 restantes.")
+	}
+	if a == Word {
+	} else {
+		Printf("Vous avez trouvé une lettre !")
+	}
+	}
+	
